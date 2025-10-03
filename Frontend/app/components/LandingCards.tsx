ca0c798460f6134dec7a749d41797ad8e72cd731
Loay Elnoamani <loaytareq44@gmail.com>
2025-10-03 15:49:10 +0300

    Add LandingPage and LandingCards components with styling

---
+++ Frontend/app/components/LandingCards.tsx
@@ -0,0 +1,26 @@
+"use client"
+import type { ReactNode } from "react";
+import "../style/landingPage.css";
+import "../globals.css";
+
+type LandingCardsProps = {
+  icon?: ReactNode;
+  header?: string;
+  info?: string;
+};
+
+function LandingCards(props: LandingCardsProps) {
+  return (
+    <>
+      
+        <div className="landing-card glass">
+          <div className="icon">{props.icon}</div>
+          <h3>{props.header}</h3>
+          <p>{props.info}</p>
+        </div>
+      
+    </>
+  );
+}
+
+export default LandingCards;
